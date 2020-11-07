ee2b61ca019971ebf50f88f4eeb8b1dc77b785d2
Alice <carrizoalice@gmail.com>
2020-11-06 23:31:39 -0300

    adding styles

--- src/main/java/com/ac/report/Controller/HomeController.java
+++ src/main/java/com/ac/report/Controller/HomeController.java
@@ -1,10 +1,15 @@
 package com.ac.report.Controller;
 
+
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.ac.report.Models.LocationStats;
 import com.ac.report.Services.CoronavirusDataService;
 
 
@@ -15,10 +20,14 @@ public class HomeController {
 	@Autowired
 	CoronavirusDataService conoraVirusDataService;
 	
+	
+	
 	@GetMapping("/")
 	public String home(Model model) {
-		
+		List<LocationStats> allStats = conoraVirusDataService.getAllStats();
+		int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
 		model.addAttribute("locationStats", conoraVirusDataService.getAllStats());
+		model.addAttribute("totalReportedCases", totalReportedCases);
 		return "home";
 	}
 }
